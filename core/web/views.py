bbcbd725d94a48788870005df586a8a0c171d6ca
AlexeyTrofimenko <byalexmail@gmail.com>
2023-10-29 15:12:34 +0200

    add dummy templates

--- core/web/views.py
+++ core/web/views.py
@@ -1,17 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from .models import Page
 
 
 def page_view(request, link):
     page = get_object_or_404(Page, link=link)
-
-    content = f"""
-    <h1>{page.name_ukr}</h1>
-    <div>{page.content_ukr}</div>
-
-    <h1>{page.name_eng}</h1>
-    <div>{page.content_eng}</div>
-    """
-
-    return HttpResponse(content)
+    return render(request, 'page.html', {'page': page})
