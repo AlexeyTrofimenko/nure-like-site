41b3747e809db43652d5614d371a1745452f207b
MagisterFelix <felix.vlad.14@gmail.com>
2023-10-31 02:50:33 +0200

    Add the ability to change the language

---
+++ core/web/static/js/scripts.js
@@ -0,0 +1,9 @@
+function setLanguageUA() {
+    Cookies.set('lang', 'ukr'); 
+    location.reload();
+}
+
+function setLanguageEN() {
+    Cookies.set('lang', 'eng'); 
+    location.reload();
+}
--- core/web/views.py
+++ core/web/views.py
@@ -5,4 +5,11 @@ from .models import Page
 
 def page_view(request, link=''):
     page = get_object_or_404(Page, link=link)
-    return render(request, 'page.html', {'page': page})
+
+    lang = request.COOKIES.get('lang', 'ukr')
+
+    response = render(request, 'page.html', {'page': page, 'lang': lang})
+    if 'lang' not in request.COOKIES:
+        response.set_cookie('lang', 'ukr')
+
+    return response
